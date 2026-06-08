3054ef754a3283533dc04ed03607ae3a31ef8b9d
Ricky Tang <tangricky001@gmail.com>
2026-06-08 14:08:27 -0400

    add q236

---
+++ 2-medium/0001-0499/0236-lowest-common-ancestor-of-a-binary-tree/main.py
@@ -0,0 +1,17 @@
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(node):
+            if not node: return None
+            if node == p or node == q: return node
+            l, r = dfs(node.left), dfs(node.right)
+            if l and r: return node
+            return l or r
+        return dfs(root)
