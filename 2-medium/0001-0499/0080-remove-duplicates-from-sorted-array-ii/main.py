4a4f8fbe5789357556928e28deb889593cfbed9e
Ricky Tang <tangricky001@gmail.com>
2026-06-23 18:59:33 -0400

    add q80

---
+++ 2-medium/0001-0499/0080-remove-duplicates-from-sorted-array-ii/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for num in nums:
+            if i == 0 or i == 1 or nums[i - 2] != num:
+                nums[i] = num
+                i += 1
+        return i
