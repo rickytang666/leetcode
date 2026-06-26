bc629663d18b9739b7ad111ca2bb101595754a23
Ricky Tang <tangricky001@gmail.com>
2026-06-26 17:50:50 -0400

    add q12

---
+++ 2-medium/0001-0499/0012-integer-to-roman/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        letters = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+
+        ans = []
+
+        for val, letter in letters:
+            if num == 0:
+                break
+            cnt = num // val
+            ans.append(cnt * letter)
+            num %= val
+        
+        return ''.join(ans)
