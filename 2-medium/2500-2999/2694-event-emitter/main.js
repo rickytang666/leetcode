dbdb089b0eb923c1cf3b14f01a9d844b08569c17
Ricky Tang <tangricky001@gmail.com>
2026-08-01 16:48:01 -0400

    add q2694

---
+++ 2-medium/2500-2999/2694-event-emitter/main.js
@@ -0,0 +1,54 @@
+class EventEmitter {
+    
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+
+    constructor() {
+        this.events = {};
+    }
+
+    subscribe(eventName, callback) {
+
+        if (!this.events[eventName])
+            this.events[eventName] = [];
+
+        this.events[eventName].push(callback);
+        
+        return {
+            unsubscribe: () => {
+                this.events[eventName] = this.events[eventName].filter(
+                    cb => cb !== callback
+                );
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        const callbacks = this.events[eventName];
+
+        if (!callbacks || callbacks.length === 0)
+            return [];
+
+        return callbacks.map(cb => cb(...args));
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
