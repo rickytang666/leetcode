ca728c499a8c8af0d47b7de05dd6c9d5a7ff380e
Ricky Tang <tangricky001@gmail.com>
2026-08-04 18:13:56 -0400

    chore: psql soln for q1757

--- 1-easy/1500-1999/1757-recyclable-and-low-fat-products/schema.sql
+++ 1-easy/1500-1999/1757-recyclable-and-low-fat-products/schema.sql
@@ -6,12 +6,7 @@ insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y')
 insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y')
 insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N')
 
-# Write your MySQL query statement below
-SELECT
-    product_id
-FROM
-    Products
-WHERE
-    low_fats = 'Y' AND recyclable = 'Y'
-ORDER BY
-    product_id
+-- Write your PostgreSQL query statement below
+select product_id
+from Products
+where low_fats = 'Y' and recyclable = 'Y'
