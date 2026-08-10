43abf11b4e744b1f8a09b2a072617eb5d15db1b4
Ricky Tang <tangricky001@gmail.com>
2026-08-10 19:16:22 -0400

    chore: add psql soln for q1068

--- 1-easy/1000-1499/1068-product-sales-analysis-I/schema.sql
+++ 1-easy/1000-1499/1068-product-sales-analysis-I/schema.sql
@@ -9,6 +9,8 @@ insert into Product (product_id, product_name) values ('100', 'Nokia')
 insert into Product (product_id, product_name) values ('200', 'Apple')
 insert into Product (product_id, product_name) values ('300', 'Samsung')
 
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 select Product.product_name, Sales.year, Sales.price
-from Sales left join Product on Sales.product_id = Product.product_id
+from Sales
+left join Product
+on Sales.product_id = Product.product_id
