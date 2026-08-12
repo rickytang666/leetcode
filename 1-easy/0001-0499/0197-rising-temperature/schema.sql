9adc67bdf16ebe5849d5c9ac3485d9780d6680aa
Ricky Tang <tangricky001@gmail.com>
2026-08-12 00:09:09 -0400

    chore: add psql soln for q197

--- 1-easy/0001-0499/0197-rising-temperature/schema.sql
+++ 1-easy/0001-0499/0197-rising-temperature/schema.sql
@@ -5,9 +5,8 @@ insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25
 insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
 insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
 
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 select w1.id
 from Weather w1
-join Weather w2
-on datediff(w1.recordDate, w2.recordDate) = 1
-where w1.temperature > w2.temperature;
+join Weather w2 on w1.recordDate = w2.recordDate + 1
+where w1.temperature > w2.temperature
