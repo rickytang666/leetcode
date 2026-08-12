e11e4546446b67798d5e0dbc85c3777180325b28
Ricky Tang <tangricky001@gmail.com>
2026-08-12 17:19:13 -0400

    chore: add psql soln for q1280

--- 1-easy/1000-1499/1280-students-and-examinations/schema.sql
+++ 1-easy/1000-1499/1280-students-and-examinations/schema.sql
@@ -23,10 +23,11 @@ insert into Examinations (student_id, subject_name) values ('13', 'Physics')
 insert into Examinations (student_id, subject_name) values ('2', 'Math')
 insert into Examinations (student_id, subject_name) values ('1', 'Math')
 
-# Write your MySQL query statement below
-select s.student_id, s.student_name, su.subject_name, count(e.subject_name) as attended_exams
-from Students s
-cross join Subjects su
-left join Examinations e on s.student_id = e.student_id and su.subject_name = e.subject_name
-group by s.student_id, s.student_name, su.subject_name
-order by s.student_id, s.student_name, su.subject_name
+-- Write your PostgreSQL query statement below
+SELECT S.student_id, S.student_name, SU.subject_name, COUNT(E.student_id) AS attended_exams
+FROM Students S
+CROSS JOIN Subjects SU
+LEFT JOIN Examinations E
+ON S.student_id = E.student_id AND SU.subject_name = E.subject_name
+GROUP BY S.student_id, S.student_name, SU.subject_name
+ORDER BY S.student_id, S.student_name, SU.subject_name
