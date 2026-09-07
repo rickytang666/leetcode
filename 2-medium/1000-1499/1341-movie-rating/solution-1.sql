45a25d419e7e22809a3e6aa5cb98bb53de81959a
Ricky Tang <rickytangdev@gmail.com>
2026-09-07 10:04:49 -0400

    add q1341

---
+++ 2-medium/1000-1499/1341-movie-rating/solution-1.sql
@@ -0,0 +1,43 @@
+-- Schema
+Create table If Not Exists Movies (movie_id int, title varchar(30));
+Create table If Not Exists Users (user_id int, name varchar(30));
+Create table If Not Exists MovieRating (movie_id int, user_id int, rating int, created_at date);
+Truncate table Movies;
+insert into Movies (movie_id, title) values ('1', 'Avengers');
+insert into Movies (movie_id, title) values ('2', 'Frozen 2');
+insert into Movies (movie_id, title) values ('3', 'Joker');
+Truncate table Users;
+insert into Users (user_id, name) values ('1', 'Daniel');
+insert into Users (user_id, name) values ('2', 'Monica');
+insert into Users (user_id, name) values ('3', 'Maria');
+insert into Users (user_id, name) values ('4', 'James');
+Truncate table MovieRating;
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '1', '3', '2020-01-12');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '2', '4', '2020-02-11');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '3', '2', '2020-02-12');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '4', '1', '2020-01-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '1', '5', '2020-02-17');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '2', '2', '2020-02-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '3', '2', '2020-03-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '1', '3', '2020-02-22');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '2', '4', '2020-02-25');
+
+-- Write your PostgreSQL query statement below
+(
+    SELECT u.name AS results
+    FROM MovieRating r
+    JOIN Users u USING (user_id)
+    GROUP BY u.name
+    ORDER BY COUNT(*) DESC, u.name ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title
+    FROM MovieRating r
+    JOIN Movies m USING (movie_id)
+    WHERE r.created_at >= '2020-02-01' AND r.created_at < '2020-03-01'
+    GROUP BY m.title
+    ORDER BY AVG(r.rating) DESC, m.title ASC
+    LIMIT 1
+);
