9ad2b2b2c0a9ca8d58a238ceb13d597ee8fd73ac
Ricky Tang <rickytangdev@gmail.com>
2026-09-14 16:30:04 -0400

    add q196

---
+++ 1-easy/0001-0499/0196-delete-duplicate-emails/solution-1.sql
@@ -0,0 +1,11 @@
+-- Schema
+Create table If Not Exists Person (Id int, Email varchar(255));
+Truncate table Person;
+insert into Person (id, email) values ('1', 'john@example.com');
+insert into Person (id, email) values ('2', 'bob@example.com');
+insert into Person (id, email) values ('3', 'john@example.com');
+
+-- Write your PostgreSQL query statement below
+DELETE FROM Person p
+USING Person q
+WHERE p.email = q.email AND p.id > q.id;
